cdd19dfce01df50124fd25870e2c7d0d99aeb345
Boon Chong Peh <pehboonchong@gmail.com>
2024-11-02 12:42:06 +0800

    update formatting

--- document/documentloader.py
+++ document/documentloader.py
@@ -1,34 +1,40 @@
-__import__('pysqlite3')
+__import__("pysqlite3")
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
+
 def load_and_split_document():
     print("loading document")
-    embeddings_model = OpenAIEmbeddings(model='text-embedding-3-large-prd-gcc2-lb', openai_api_base="https://litellm.govtext.gov.sg/")
+    embeddings_model = OpenAIEmbeddings(
+        model="text-embedding-3-large-prd-gcc2-lb", openai_api_base="https://litellm.govtext.gov.sg/"
+    )
     pdf_path = "document/document.pdf"
     loader = PyPDFLoader(pdf_path)
     pages = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter(
-    separators=["\n\n", "\n", " ", ""],
-    chunk_size=500,
-    chunk_overlap=50
-)
-    
+        separators=["\n\n", "\n", " ", ""], chunk_size=500, chunk_overlap=50
+    )
+
     splitted_documents = text_splitter.split_documents(pages)
-    vectordb = Chroma.from_documents(splitted_documents, embeddings_model, collection_name='embedding_semantic', persist_directory='./vector_db')
-    
+    vectordb = Chroma.from_documents(
+        splitted_documents, embeddings_model, collection_name="embedding_semantic", persist_directory="./vector_db"
+    )
+
     return vectordb
 
+
 def load_document():
-    embeddings_model = OpenAIEmbeddings(model='text-embedding-3-large-prd-gcc2-lb', openai_api_base="https://litellm.govtext.gov.sg/")
+    embeddings_model = OpenAIEmbeddings(
+        model="text-embedding-3-large-prd-gcc2-lb", openai_api_base="https://litellm.govtext.gov.sg/"
+    )
     pdf_path = "document/document.pdf"
     loader = PyPDFLoader(pdf_path)
     pages = loader.load()
     return pages
-    
--- pages/aboutus.py
+++ pages/aboutus.py
@@ -1,15 +1,16 @@
-import streamlit as st 
+import streamlit as st
 
 
-st.set_page_config(
-    layout="centered",
-    page_title="About us"
-)
+st.set_page_config(layout="centered", page_title="About us")
 
 st.title("About Us")
 
-st.write(f"""We are Air Force Engineer from the Republic of Singapore Air Force.  Our primary job is to maintain and ensure that the aircraft is fit for flying to protect Singapore from adversary threats. 
-             The new maintenance crew members are required to learn the maintenance manual as part of their on-job training and induction to the maintenance flight. 
+st.write(
+    f"""We are Air Force Engineer from the Republic of Singapore Air Force.  Our primary job is to maintain and ensure that the aircraft is fit for flying to protect Singapore from adversary threats.
+             The new maintenance crew members are required to learn the maintenance manual as part of their on-job training and induction to the maintenance flight.
              Currently, the crew members feel that the manual is hard to understand as there is too much content. Also, searching the manual to answer our doubt is very difficult as there is too much content to look through.
-             Hence, the goal of this project is to make learning of the materials easier to understand for on job trainings. We are intending to use AI to accelerate their learnings, making the induction process easier.""")
-st.write(f"""The AI will answer questions from the OJTs when they ask anything related to the materials. E.g what are the dimensions of the F15-SG? The bot would answer accordingly. Since it’s an AI bot, if the user were to prompt “Quiz me anything about the Fuel/ECS system of the F15-SG”, the bot should be able to operate like a “quiz bot”, guiding the user on the correct answer and all that. Overall, this AI bot should be able to act like a teacher towards the user paraphrasing technical jargon, answering questions and quizzing would be the prime functions it should be able to execute.""")
+             Hence, the goal of this project is to make learning of the materials easier to understand for on job trainings. We are intending to use AI to accelerate their learnings, making the induction process easier."""
+)
+st.write(
+    f"""The AI will answer questions from the OJTs when they ask anything related to the materials. E.g what are the dimensions of the F15-SG? The bot would answer accordingly. Since it’s an AI bot, if the user were to prompt “Quiz me anything about the Fuel/ECS system of the F15-SG”, the bot should be able to operate like a “quiz bot”, guiding the user on the correct answer and all that. Overall, this AI bot should be able to act like a teacher towards the user paraphrasing technical jargon, answering questions and quizzing would be the prime functions it should be able to execute."""
+)
